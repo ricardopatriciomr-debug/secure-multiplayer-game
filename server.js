a2d50661da8d7cd48406573b74c808564c5f67be
ricardopatriciomr-debug <ricardopatricio.mr@gmail.com>
2025-11-18 22:47:32 -0300

    Move nocache middleware to a different position
    "Fix nocache middleware order"

--- server.js
+++ server.js
@@ -12,10 +12,10 @@ const runner = require('./test-runner.js');
 
 const app = express();
 
-app.use(nocache());
 app.use(helmet.noSniff());
 app.use(helmet.xssFilter());
 app.use(helmet.hidePoweredBy({ setTo: 'PHP 7.4.3' }));
+app.use(nocache());
 
 app.use('/public', express.static(process.cwd() + '/public'));
 app.use('/assets', express.static(process.cwd() + '/assets'));
